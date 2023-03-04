b285961273ed8448a74197b9c27ee5c9b0ae44bb
小川敦司 <>
2023-03-04 17:29:57 +0900

    javascriptを用いて、画像選択と同時に画像アップロード機能実装

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,7 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 
+
 import $ from 'jquery'
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
@@ -16,9 +17,23 @@ import $ from 'jquery'
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
 
-$('input[type=file]').on('change', function (e) {
-    console.log(e.target.files);
-    if (e.target.files[0].size > 0) {
-        $(this).parents('form').submit();
-    }
+window.addEventListener('turbolinks:load', () => {
+    $('input[type=file]').on('change', function (e) {
+        console.log(e.target.files);
+        if (e.target.files[0].size > 0) {
+            $(this).parents('form').submit();
+        }
+    });
+    // const uploader = document.querySelector('.uploader');
+    // uploader.addEventListener('change', (e) => {
+    //   const file = uploader.files[0];
+    //   const reader = new FileReader();
+    //   reader.readAsDataURL(file);
+    //   reader.onload = () => {
+    //     const image = reader.result;
+    //     document.querySelector('.default-avatar.png').setAttribute('src', image);
+    //   }
+    //   $('.submit-botton').removeClass('hidden')
+    // });
 });
+
