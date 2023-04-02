ef5abc7b97140fa5b465ad574cd5e8dde09501f4
小川敦司 <>
2023-04-02 18:24:57 +0900

    簡易版メール通知機能を実装

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,4 +2,11 @@ class Comment < ApplicationRecord
     belongs_to :article
     belongs_to :user
     validates :content, presence: true
+
+    after_create :send_email
+
+    private
+    def send_email
+        CommentMailer.new_comment(user).deliver_later
+    end
 end
