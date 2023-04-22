674171296131a17e16fa0e091a462ce92669c42b
小川敦司 <>
2023-04-22 17:29:13 +0900

    follower、followingの数を表示するよう設定

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -3,6 +3,8 @@ class AccountsController < ApplicationController
     def show
         @user = User.find(params[:id])
         @articles = @user.articles
+        @follower_count = @user.followers.count
+        @following_count = @user.followings.count
     end
 
 end
