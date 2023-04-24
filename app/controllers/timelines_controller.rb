a4426e574f1b80a57c8cdd809fb4f00cc45b28f5
小川敦司 <>
2023-04-25 07:42:11 +0900

    記事数を５件に制限

--- app/controllers/timelines_controller.rb
+++ app/controllers/timelines_controller.rb
@@ -4,7 +4,7 @@ class TimelinesController < ApplicationController
     def show
       user_ids = current_user.followings.pluck(:id)
       @articles = Article.where(user_id: user_ids)
-      @liked_articles = Article.joins(:likes).group("articles.id").order("count(articles.id) DESC")
+      @liked_articles = Article.joins(:likes).group("articles.id").order("count(articles.id) DESC").limit(5)
       binding.pry
     end
 
