a07e0720a0c0884b1fc5fcc8919ca96668fd6cce
小川敦司 <>
2023-04-25 19:20:48 +0900

    いいね機能をviewsに反映

--- app/controllers/timelines_controller.rb
+++ app/controllers/timelines_controller.rb
@@ -5,7 +5,6 @@ class TimelinesController < ApplicationController
       user_ids = current_user.followings.pluck(:id)
       @articles = Article.where(user_id: user_ids)
       @liked_articles = Article.joins(:likes).group("articles.id").order("count(articles.id) DESC").limit(5)
-      binding.pry
     end
 
 end
