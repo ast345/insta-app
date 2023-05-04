f716b882b0703193727a9cc86ad062fe7b78020a
小川敦司 <>
2023-05-04 15:32:07 +0900

    configのsytaxエラーに対処

--- config/application.rb
+++ config/application.rb
@@ -16,6 +16,7 @@ module InstaApp
     if Rails.env.development? || Rails.env.test?
       Bundler.require(*Rails.groups)
       Dotenv::Railtie.load
+    end
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
