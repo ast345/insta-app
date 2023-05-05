d0bd08eafb17c2dc45008b37910f5c03851ded88
小川敦司 <>
2023-05-05 17:33:57 +0900

    SendGridを使用するための設定

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,9 +60,19 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "insta_app_production"
 
-  config.action_mailer.perform_caching = false
-  config.action_mailer.default_url_options = { host: 'insta-app.herokuapp.com' }
-
+  # config.action_mailer.perform_caching = false
+  # config.action_mailer.default_url_options = { host: 'insta-app.herokuapp.com' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.sendgrid.net',
+    port:                 587,
+    authentication:       :plain,
+    user_name:            'apikey',
+    password:             ENV['SENDGRID_API_KEY'],
+    domain:               'heroku.com',
+    enable_starttls_auto: true
+  }
+  
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
