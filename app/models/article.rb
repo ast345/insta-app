7038a95fb3a65adc2fc25f519cc4e6b94e1ce4c2
小川敦司 <>
2023-05-05 10:30:45 +0900

    BlobValidatorをコメントアウト

--- app/models/article.rb
+++ app/models/article.rb
@@ -4,7 +4,7 @@ class Article < ApplicationRecord
     has_many :likes, dependent: :destroy
     has_many :comments, dependent: :destroy
     # scope :featured, order('likes_count DESC')
-    validates_with BlobValidator
+    # validates_with BlobValidator
     validates :sentence, length: {minimum: 2}
     validates :photos, presence: true, blob: { content_type: :image }
 end
