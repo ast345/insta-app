c29a9a95355a4bdd7b12cabd5d50c94a484c6255
小川敦司 <>
2023-05-05 18:14:12 +0900

    送信元のメールアドレスを登録

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,6 +62,7 @@ Rails.application.configure do
 
   # config.action_mailer.perform_caching = false
   # config.action_mailer.default_url_options = { host: 'insta-app.herokuapp.com' }
+  config.action_mailer.default_options = { from: 'charlie.a152@gmail.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address:              'smtp.sendgrid.net',
